6058bc2db3718c1aac860fc1ba65b712f21c1ec3
eriqueg <guizanoni1234@gmail.com>
2019-05-29 12:31:41 -0300

    subindo o codigo

---
+++ main.js
@@ -0,0 +1,121 @@
+function transfDec(vetor){
+    for(x = 0; x< vetor.length; x++){
+        vetor[x] = parseFloat(vetor[x]);
+    }
+    return vetor;
+}
+
+
+function ordenarSelecao (){
+    var t0 = performance.now();
+    var trocas = 0;
+    var comparacoes = 0;
+    var passos = 0;
+    var menor;
+    var posicao;
+    var vetor = []
+    let texto = document.getElementById("numeros").value;
+    vetor = texto.split(",");
+    transfDec(vetor)
+    var posiAnt;
+    isTroca = false;
+    console.log("----------------ORDENAÇÃO POR SELEÇÃO-----------");
+    console.log("Vetor não ordenado: ",vetor)
+    
+
+    for(x = 0; x < vetor.length; x++){
+        passos = passos + 1;
+        menor = vetor[x];
+        isTroca = false;
+        for(y = x+1; y < vetor.length; y++){
+            if(vetor[y] < menor){
+                posicao = y;
+                menor = vetor[y];
+                isTroca = true;
+                posiAnt = vetor[y];
+
+            }
+            comparacoes++;
+        }
+
+        if(isTroca){
+            vetor[posicao] = vetor[x];
+            vetor[x] = posiAnt;
+            trocas = trocas + 1;
+        }
+    }
+    var t1 = performance.now();
+    var tempo = t1 - t0;
+
+    document.getElementById("ordenados").innerHTML = vetor;
+    passos = passos - 1;
+    document.getElementById("troc").innerHTML = "Trocas realizadas: " + trocas;
+    document.getElementById("comp").innerHTML = "Comparacoes realizadas: " + comparacoes;
+    document.getElementById("pass").innerHTML = "Passos necessários: " + passos;
+    document.getElementById("temp").innerHTML = "Tempo decorrido: " + tempo;
+
+    let data = new Date;
+    localStorage.setItem(formatarData(data), vetor);
+
+    console.log("trocas: ", trocas);
+    console.log("Comparações: ", comparacoes);
+    console.log("Passos: ", vetor.length - 1);
+    console.log("Vetor Ordenado: ",vetor);
+    console.log("Tempo decorrido: ", tempo)
+}
+
+
+function ordenarBolha(){
+    var t0 = performance.now();
+    var cont = 0;
+    var trocas = 0;
+    var pesquisa = 0;
+    var vetor = []
+    let texto = document.getElementById("numeros").value;
+    vetor = texto.split(",");
+    transfDec(vetor)
+    var compVet = vetor.length - 1;
+    console.log("----------------ORDENAÇÃO POR BOLHA-----------");
+    console.log("Vetor não ordenado:",vetor)
+    for(x = 0; x < vetor.length; x++ ){
+        cont = cont +1;
+        for(y = 0; y < compVet; y++){
+            
+            if(vetor[y] > vetor[y + 1]){
+                var posiAnt = vetor[y + 1]
+                vetor[y + 1] = vetor[y]
+                vetor[y] = posiAnt;
+                trocas = trocas + 1;
+                pesquisa = pesquisa + 1;
+            
+                
+            }else{
+                pesquisa = pesquisa + 1;
+            }
+        }
+        compVet = compVet - 1;
+    }
+    var t1 = performance.now();
+    var tempo = t1 - t0;
+    document.getElementById("ordenados").innerHTML = vetor;
+    let passos = cont - 1;
+    document.getElementById("troc").innerHTML = "Trocas realizadas: " + trocas;
+    document.getElementById("comp").innerHTML = "Comparacoes realizadas: " + pesquisa;
+    document.getElementById("pass").innerHTML = "Passos necessários: " + passos;
+    document.getElementById("temp").innerHTML = "Tempo decorrido: " + tempo;
+    let data = new Date;
+    localStorage.setItem(formatarData(data), vetor);
+
+    console.log("Passos:",cont-1);
+    console.log("Trocas:",trocas);
+    console.log("Pesquisa:",pesquisa);
+    console.log("Vetor ordenado:",vetor);
+    console.log("Tempo decorrido: ", tempo)
+}
+
+function formatarData(data){
+    return data.getDate() + "/" + data.getMonth() + "/" + data.getFullYear();
+}
+
+/*ordenarSelecao();
+ordenarBolha();*/
